4aae4f17e58b2becaf1f3317da861a6eff0046b3
Prajakta Gaikwad <gaikwadprajakta699@gmail.com>
2026-09-14 17:46:11 +0530

    Project setup

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8081",
+  baseURL: "https://consistiq-backend.onrender.com",
 });
 
 // =======================
